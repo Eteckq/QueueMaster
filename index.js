9ea6447cb203ed6721edffbb7a41c9509864e7ec
Yohan <yohan.gastoud@patrowl.io>
2025-12-02 11:19:16 +0100

    Update

--- index.js
+++ index.js
@@ -1,114 +1,169 @@
 const puppeteer = require("puppeteer");
 const log = require("log-beautify");
-var prompt = require("prompt");
+const readline = require("readline");
 
-async function start(instances, url, debug) {
-  let bestPlace = null;
-  let consumed = 0;
-  for (let i = 0; i < instances; i++) {
-    createQueue(url, debug)
-      .then((queue) => {
-        if (!bestPlace) {
-          log.show("\n");
-          bestPlace = queue.position;
-        }
-        if (queue.position < bestPlace) {
-          bestPlace = queue.position;
-          log.success(
-            `Better place found! (${i}) - ${queue.position} => ${queue.url}`
-          );
-        } else {
-          log.show(
-            `New place found (${i}) - ${queue.position} => ${queue.url}`
-          );
-        }
-      })
-      .catch(() => {
-        log.warning(`Queue ${i} failed`);
-      })
-      .finally(async () => {
-        consumed += 1;
-        if (instances === consumed) {
-          log.info(`Over ! The best place is ${bestPlace}`);
-          await prompt.get("press enter to exit");
-        }
-      });
+class QueueMaster {
+  constructor() {
+    this.bestPlace = null;
+    this.queueCount = 0;
+    this.activeQueues = new Set();
   }
 
-  log.info(`Started ${instances} queues`);
-}
+  async createQueue(url) {
+    const queueId = this.queueCount++;
+    this.activeQueues.add(queueId);
 
-async function createQueue(url, debug) {
-  return new Promise(async (resolve, reject) => {
-    const browser = await puppeteer.launch({ headless: debug ? false : true });
-    const page = await browser.newPage();
+    return new Promise(async (resolve, reject) => {
+      let browser = null;
+      let page = null;
 
-    await page.goto(url);
+      try {
+        browser = await puppeteer.launch({
+          headless: true,
+          args: ["--no-sandbox"],
+        });
+        page = await browser.newPage();
 
-    await page
-      .waitForSelector(".erreurback", { timeout: 1000 })
-      .then(() => {
-        reject();
-      })
-      .catch(async () => {
+        browser.on("disconnected", () => {
+          reject(new Error("Browser was closed manually"));
+        });
+
+        await page.goto(url);
+
+        try {
+          await page.waitForSelector(".erreurback", { timeout: 1000 });
+          reject(new Error("Error page detected"));
+          return;
+        } catch {
+        }
+
         await page.waitForSelector("#MainPart_lbUsersInLineAheadOfYou", {
           timeout: false,
           visible: true,
         });
 
-        const res = await page.evaluate(() => {
+        const result = await page.evaluate(() => {
+          const positionElement = document.querySelector(
+            "#MainPart_lbUsersInLineAheadOfYou"
+          );
+          const idElement = document.querySelector("#hlLinkToQueueTicket2");
+
+          if (!positionElement || !idElement) {
+            return null;
+          }
+
           return {
-            position: parseInt(
-              document.querySelector("#MainPart_lbUsersInLineAheadOfYou")
-                .innerHTML
-            ),
-            id: document.querySelector("#hlLinkToQueueTicket2").innerHTML,
+            position: parseInt(positionElement.innerHTML),
+            id: idElement.innerHTML,
           };
         });
-        const url = new URL(page.url());
+
+        if (!result || !result.position || !result.id) {
+          reject(new Error("Required selectors not found"));
+          return;
+        }
+
+        const currentUrl = new URL(page.url());
         resolve({
-          ...res,
-          url: `${url.origin}/?c=${url.searchParams.get(
+          ...result,
+          url: `${currentUrl.origin}/?c=${currentUrl.searchParams.get(
             "c"
-          )}&e=${url.searchParams.get("e")}&q=${res.id}`,
+          )}&e=${currentUrl.searchParams.get("e")}&q=${result.id}`,
         });
+      } catch (error) {
+        reject(error);
+      } finally {
+        this.activeQueues.delete(queueId);
+        try {
+          if (browser && browser.isConnected()) {
+            await browser.close();
+          }
+        } catch {
+        }
+      }
+    });
+  }
+
+  handleQueueResult(queueId, queue) {
+    if (!this.bestPlace) {
+      log.show("\n");
+      this.bestPlace = queue.position;
+    }
+
+    if (queue.position < this.bestPlace) {
+      this.bestPlace = queue.position;
+      log.success(
+        `Better place found! (Queue #${queueId}) - Position: ${queue.position} => ${queue.url}`
+      );
+    } else {
+      log.show(
+        `New place found (Queue #${queueId}) - Position: ${queue.position} => ${queue.url}`
+      );
+    }
+  }
+
+  handleQueueError(queueId, error) {
+    log.warning(`Queue #${queueId} failed: ${error.message}`);
+  }
+
+  async spawnQueue(url) {
+    const queueId = this.queueCount;
+
+    this.createQueue(url)
+      .then((queue) => {
+        this.handleQueueResult(queueId, queue);
+      })
+      .catch((error) => {
+        this.handleQueueError(queueId, error);
       });
+  }
 
-    await browser.close();
-  });
+  async start(url) {
+    log.info("QueueMaster started. Press Enter to create a new queue.");
+    log.info(`Current best place: ${this.bestPlace || "N/A"}`);
+    log.info(`Active queues: ${this.activeQueues.size}`);
+  }
 }
-log.info(
-  `This script only works for events protected by queue-it, like ticketmaster`
-);
 
-prompt.start({
-  message: " ",
-  delimiter: " ",
-});
-prompt
-  .get([
-    {
-      name: "debug",
-      message: "Set the debug mode (skip to ignore)",
-    },
-    {
-      name: "queues",
-      message: "How many queue to you want to create ?",
-      validator: /^[0-9\s-]+$/,
-      type: "number",
-      default: 10,
-      required: true,
-      warning: "You need to enter a number",
-    },
-    {
-      name: "url",
-      message:
-        "URL of the event",
-      default:
-        "https://www.ticketmaster.fr/fr/manifestation/the-weeknd-billet/idmanif/541693",
-      required: true,
-    },
-  ])
-  .then(async (res) => {
-    start(res.queues, res.url, res.debug);
+async function main() {
+  log.info(
+    "This script only works for events protected by queue-it, like ticketmaster"
+  );
+
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
   });
+
+  const question = (prompt) =>
+    new Promise((resolve) => rl.question(prompt, resolve));
+
+  try {
+    const url =
+      (await question("Enter the event URL: ")) ||
+      "https://www.ticketmaster.fr/fr/manifestation/the-weeknd-billet/idmanif/541693";
+
+    log.info(`Using URL: ${url}\n`);
+
+    const queueMaster = new QueueMaster();
+    await queueMaster.start(url);
+
+    while (true) {
+      await question("Press Enter to create a new queue (or Ctrl+C to exit): ");
+      await queueMaster.spawnQueue(url);
+      log.info(
+        `Queue #${queueMaster.queueCount - 1} spawned`
+      );
+    }
+  } catch (error) {
+    if (error.code === "SIGINT" || error.message.includes("SIGINT")) {
+      log.info("\nExiting...");
+      process.exit(0);
+    }
+    log.error(`Error: ${error.message}`);
+  } finally {
+    rl.close();
+  }
+}
+
+main();
--- readme.md
+++ readme.md
@@ -2,6 +2,26 @@
 
 This script only works for events protected by queue-it, like ticketmaster
 
+*Update 12/2025*
+
+This script doesn't works as well as before. You need to solve captchas manually
+
+
+
+# Installation
+
+- npm i
+- You need to install pupetter, node will try to install it but sometimes you need some extra manuals steps
+
+# Usage
+
+- npm run start
+- Enter url of the event
+- Press enter to create a new queue
+- Solve captcha
+- You can now spawn an other queue if you want
+- Once the place is found, browser will close, and you will get the url to go to get the queue
+
 # Demo
 
 <img src="demo.gif">
